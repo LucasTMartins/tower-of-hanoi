ec558a1af4aa382b7440deff576e4cd29e056bb8
Lucas Martins de Oliveira <lucasm236954@gmail.com>
2023-05-24 15:47:11 -0300

    v3

--- src/main/java/Answer.java
+++ src/main/java/Answer.java
@@ -7,8 +7,8 @@ public class Answer {
     Stack<Integer> s3 = new Stack<>();
     Integer rm_value;
     Integer rm_stack;
-    
-    //Roda o jogo em si
+
+    // Roda o jogo em si
     public void menu(int stack_num) {
         switch (stack_num) {
             case 1:
@@ -19,7 +19,7 @@ public class Answer {
                 rm_value = checkEmpty();
                 putValue(stack_num, rm_value);
                 break;
-                
+
             case 2:
                 if (s2.size() >= 3) {
                     System.out.println("A pilha 2 está cheia!");
@@ -42,46 +42,46 @@ public class Answer {
                 break;
         }
     }
-    
+
     public int checkEmpty() {
         Scanner sc = new Scanner(System.in);
-        
+
         // Verificando o item a ser retirado da fila
         System.out.println("Digite a fila do qual será retirado:");
-        rm_stack= sc.nextInt();
-        if (rm_stack== 1 && s1.isEmpty() == false) {
+        rm_stack = sc.nextInt();
+        if (rm_stack == 1 && s1.isEmpty() == false) {
             return s1.peek();
-        } else if (rm_stack== 2 && s2.isEmpty() == false) {
+        } else if (rm_stack == 2 && s2.isEmpty() == false) {
             return s2.peek();
-        } else if (rm_stack== 3 && s3.isEmpty() == false) {
+        } else if (rm_stack == 3 && s3.isEmpty() == false) {
             return s3.peek();
         }
         return 0;
     }
-    
+
     public void putValue(int stack_num, int value) {
         boolean error_check = false;
 
         // Add o valor removido (rm_value) da pilha de remoção (rm_stack)
         switch (stack_num) {
             case 1:
-                if (value >=1 && (s1.isEmpty() || s1.peek() > value)) {
+                if (value >= 1 && (s1.isEmpty() || s1.peek() > value)) {
                     s1.add(value);
-                } else{
+                } else {
                     error_check = true;
                 }
                 break;
             case 2:
-                if (value >=1 && (s2.isEmpty() || s2.peek() > value)) {
+                if (value >= 1 && (s2.isEmpty() || s2.peek() > value)) {
                     s2.add(value);
-                }else{
+                } else {
                     error_check = true;
                 }
                 break;
             case 3:
-                if (value >=1 && (s3.isEmpty() || s3.peek() > value)) {
+                if (value >= 1 && (s3.isEmpty() || s3.peek() > value)) {
                     s3.add(value);
-                } else{
+                } else {
                     error_check = true;
                 }
                 break;
@@ -89,15 +89,16 @@ public class Answer {
             default:
                 break;
         }
-        
-        // Checando se não há nenhum erro para retirar o valor da pilha de remoção (rm_stack)
+
+        // Checando se não há nenhum erro para retirar o valor da pilha de remoção
+        // (rm_stack)
         if (error_check) {
             System.out.println("Erro encontrado, algo não se encaixa!");
-        } else if(rm_stack == 1){
+        } else if (rm_stack == 1) {
             s1.pop();
-        } else if(rm_stack == 2){
+        } else if (rm_stack == 2) {
             s2.pop();
-        } else if(rm_stack == 3){
+        } else if (rm_stack == 3) {
             s3.pop();
         }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
+
 public class Main {
 
-    
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Answer num = new Answer();
         num.s1.push(3);
         num.s1.push(2);
         num.s1.push(1);
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("\nMova a pilha um para a pilha dois, uma peça de cada vez! Nunca com uma peça maior em cima da menor!");
+        System.out.println(
+                "\nMova a pilha um para a pilha dois, uma peça de cada vez! Nunca com uma peça maior em cima da menor!");
         System.out.println("\nTorres: \nTorre 1: " + num.s1 + "\nTorre 2: " + num.s2 + "\nTorre 3: " + num.s3);
         while (true) {
             if (num.s2.search(3) == 3 && num.s2.search(2) == 2 && num.s2.search(1) == 1) {
                 System.out.println("\nParabéns, você conseguiu!");
                 break;
-            }
-            else {
+            } else {
                 System.out.println("\nDigite a pilha que deseja adicionar um valor:");
                 num.menu(sc.nextInt());
             }
